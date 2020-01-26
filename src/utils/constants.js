ae321efa02245c52146975b28ce5051053e4e6be
Eduardo Rodrigues <eduardo.rodrigues@sentia.com>
2020-01-26 19:52:33 +0100

    refactor and simplification

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,27 +1,5 @@
-const cookieOptions = {
-    uri: 'https://www.linkedin.com/uas/authenticate',
-    headers: {
-        "X-Li-User-Agent": "LIAuthLibrary:3.2.4 com.linkedin.LinkedIn:8.8.1 iPhone:8.3",
-        "User-Agent": "LinkedIn/8.8.1 CFNetwork/711.3.18 Darwin/14.0.0",
-        "X-User-Language": "en",
-        "X-User-Locale": "en_US",
-        "Accept-Language": "en-us",
-    },
-    method: "GET"
-};
-
-const getURIOptions = {
-    uri: "https://www.linkedin.com/sales/gmail/profile/proxy",
-    headers: {
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36",
-        "accept-language": "en-AU,en-GB;q=0.9,en-US;q=0.8,en;q=0.7",
-        "x-li-lang": "en_US",
-        "x-restli-protocol-version": "2.0.0",
-    },
-    method: 'GET',
-}
+const LI_CDN = 'https://media-exp2.licdn.com/media';
 
 module.exports = {
-    getURIOptions, 
-    cookieOptions
+    LI_CDN,
 }
